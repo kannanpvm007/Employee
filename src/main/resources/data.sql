9171a39a0a32f3fe2c2ae4053cbcb9930be9eb10
Kannan Ramakrishnan <kannan.r@kgisl.com>
2019-07-26 15:24:28 +0530

    15  commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,12 @@
-insert into Employee (id,name) values (2,'mohana');
-insert into Employee (id,name) values (3,'ramya');
+-- insert into Employee (id,name) values ("mohana");
+-- insert into Employee (id,name) values ("ramya");
 
-insert into Employee (id,name) values (4,'mani');
+-- insert into Employee (id,name) values ("mani");
 
-insert into Employee (id,name) values (1,'raj');
+-- insert into Employee (id,name) values ("raj");
 
+insert into Employee (id,name) values (1,'Honda');
+insert into Employee (id,name) values (2,'Hyndai');
+insert into Employee (id,name) values (3,'Ferrari');
+insert into Employee (id,name) values (4,'BMW');
+insert into Employee (id,name) values (5,'Audi');
